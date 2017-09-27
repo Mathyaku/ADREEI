d61ce9440d2be4c57d0bf8282b770c691aada63c
mathyaku <mathyaku@gmail.com>
2017-09-26 21:19:27 -0300

    Redefinitions of the size of imgs

---
+++ dev/scripts/resizeImage.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 19:48:20 2017
+
+@author: Matheus
+"""
+import PIL
+from PIL import Image
+from scipy import misc
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+
+def storeImages():
+   
+    # importing all name of imgs
+    pictureNames = os.listdir("../train")
+    
+    # creating the directory
+    os.makedirs('../vba')
+    
+    for imgName in pictureNames:
+        
+        # read img
+        img = Image.open("../train/" + imgName)
+    
+        # setting dimensions
+        width = 640
+        height = 480
+        
+        # resizing the img
+        imgResized = img.resize((width, height), Image.NEAREST)
+        
+        #converting img to array
+        imgRGB = np.array(imgResized)
+        
+        # save resized image 
+        misc.imsave("../vba/" + imgName, imgRGB)
+        
+        # visualization
+        #plt.imshow(imgRGB)
+        
+    return
