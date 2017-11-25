49cf976afae27f55c2678fc8d14cc6468028cade
mathyaku <mathyaku@gmail.com>
2017-11-25 11:25:36 -0200

    Novo modelo de index

--- dev/scripts/index.py
+++ dev/scripts/index.py
@@ -6,16 +6,25 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame
 from statistics import median
+import random
 
 import loadModel
+import modelagem
 
 
 # mean and standard deviation
-mu_lat, sigma_lat = -4.17, 3
-mu_long, sigma_long = -62, 3
 
 df = pd.read_pickle("positions.pkl")
 df = df.loc[1:689,:]
+
+s_lat = df.loc[:,1]
+s_long = df.loc[:,0]
+
+scores = []
+
+iInvasive = []
+iNonInvasive = []
+
 
 app = dash.Dash('Hello World')
 
@@ -29,7 +38,9 @@ app.layout = html.Div([
                 id='my-dropdown',
                 options=[
                     {'label': 'Treinar', 'value': 'train'},
+                    {'label': 'Invasoras', 'value': 'invasive'},
                     {'label': 'Gerar Rotas', 'value': 'route'},
+                    {'label': 'Clusterização', 'value': 'cluster'},
                     {'label': 'Limpar', 'value': 'clean'}
                 ],
                 value='COKE'
@@ -45,35 +56,74 @@ app.layout = html.Div([
 
 
 
-
 @app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
 def update_graph(selected_dropdown_value):
+    global scores
+    global s_lat
+    global s_long
+    global iInvasive
+    global iNonInvasive
+    
     if(selected_dropdown_value == 'train'):
-        scores = loadModel.predict()
-        print(scores)
+        
+        scores = [0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+        #loadModel.predict()
+        iInvasive = [i for i, x in enumerate(scores) if x == 1]
+        iNonInvasive = [i for i, x in enumerate(scores) if x == 0]
+        
+        data = [{
+            'name': 'Invasora',
+            'lat': s_lat[iInvasive], 
+            'lon': s_long[iInvasive], 
+            'type': 'scattermapbox',
+            'marker': dict( size=5, color='rgb(200,0,0)')
+        },
+        {
+            'name': 'Não Invasora',
+            'lat': s_lat[iNonInvasive], 
+            'lon': s_long[iNonInvasive], 
+            'type': 'scattermapbox',
+            'marker': dict( size=5, color='rgb(0,200,0)')
+        }]
+    elif(selected_dropdown_value == 'invasive'):
+        data = [{
+            'name': 'Invasora',
+            'lat': s_lat[iInvasive], 
+            'lon': s_long[iInvasive], 
+            'type': 'scattermapbox',
+            'marker': dict( size=5, color='rgb(200,0,0)')
+        }]
     elif(selected_dropdown_value == 'route'):
-        s_lat=df.loc[:,1]
-        s_long=df.loc[:,0]
+        modelagem.gen_paths(iInvasive)
+        data = [{
+            'name': 'Invasora',
+            'lat': s_lat[iInvasive], 
+            'lon': s_long[iInvasive], 
+            'type': 'scattermapbox',
+            'marker': dict( size=5, color='rgb(200,0,0)')
+        }]   
+    elif(selected_dropdown_value == 'cluster'):
+        chunckOfCluster = np.array_split(np.array(iInvasive),6)
+        data = []
+        for i in range(6) :
+            data.append({
+                'name': 'Cluster ' + str(i),
+                'lat': s_lat[chunckOfCluster[i]], 
+                'lon': s_long[chunckOfCluster[i]], 
+                'type': 'scattermapbox',
+                'marker': dict( size=5, color='rgb('+str(random.randint(0,255))+','+str(random.randint(0,255))+','+str(random.randint(0,255))+')')
+            })
     else:
-        s_lat=df.loc[:,1]
-        s_long=df.loc[:,0]
-        rgb = [0,0,200]
+        data = [{
+                'name': 'Não identificado',
+                'lat': s_lat[1:689], 
+                'lon': s_long[1:689], 
+                'type': 'scattermapbox',
+                'marker': dict( size=5, color='rgb(0,0,200)')
+            }]
     
     return {
-            'data': [{
-                'name': 'Invasora',
-                'lat': s_lat[1:122], 
-                'lon': s_long[1:122], 
-                'type': 'scattermapbox',
-                'marker': dict( size=5, color='rgb('+str(rgb[0])+',' +str(rgb[1])+',' +str(rgb[2])+')')
-            },
-            {
-                'name': 'Não Invasora',
-                'lat': s_lat[122:], 
-                'lon': s_long[122:],
-                'type': 'scattermapbox',
-                'marker': dict( size=5, color='rgb('+str(rgb[0])+',' +str(rgb[1])+',' +str(rgb[2])+')')
-            }],
+            'data': data,
         'layout': {
             'mapbox': {
                 'accesstoken': (
